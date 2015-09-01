e2b638aef50c21fedf13fc1dd30c8895ba3d42d4
rajat70 <rajatgangwar1234@gmail.com>
2015-09-01 18:56:03 +0530

    LoginDummy

---
+++ app/src/main/java/com/test/rajat/minivie/MyApplication.java
@@ -0,0 +1,40 @@
+package com.test.rajat.minivie;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Rajat on 8/10/2015.
+ */
+public class MyApplication extends Application {
+    PackageInfo info;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            info = getPackageManager().getPackageInfo("com.you.name", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
+    }
+}
